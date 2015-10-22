e1bcc91d4ab3ff4fd983744277ca1a9f009cd171
Stefan Valentin <khalilravanna@gmail.com>
2015-10-22 11:13:42 -0700

    Added node server to serve page. Added react

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+/**
+ * Main application file
+ */
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Application Config
+// var config = require('./lib/config/config');
+
+// Connect to database
+// var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+
+// Bootstrap models
+// var modelsPath = path.join(__dirname, 'lib/models');
+// fs.readdirSync(modelsPath).forEach(function (file) {
+//   if (/(.*)\.(js$|coffee$)/.test(file)) {
+//     require(modelsPath + '/' + file);
+//   }
+// });
+
+// Populate empty DB with sample data
+// require('./lib/config/dummydata');
+  
+// Passport Configuration
+// var passport = require('./lib/config/passport');
+
+var app = express();
+
+// Express settings
+require('./express-config')(app);
+
+// Routing
+// require('./lib/routes')(app);
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+// Start server
+app.listen(9000, function () {
+});
+
+// Expose app
+exports = module.exports = app;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1>revtube</h1>,
+  document.getElementById('main')
+);

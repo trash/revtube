db142fec3d4801e90545114d1ae9293033286fba
Den Markin <den@rev.com>
2015-10-22 14:00:50 -0700

    Style the sidebar

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ var ParseTestComponent = window.ParseTestComponent,
 		render: function () {
 			return (
 				<div className="main">
-				    <h3 className="masthead-brand">PartyTube</h3>
-                    <nav></nav>
 					<PlaylistComponent playlistId={ 'vxLtIafOqa' }/>
 				</div>
 			);
--- src/playlist-component.js
+++ src/playlist-component.js
@@ -7,12 +7,19 @@ var PlaylistItemComponent = React.createClass({
 	},
 	render: function () {
 		var playlistItem = this.props.playlistItem;
+		var videoPicStyle = {
+		  backgroundImage: 'url(' + playlistItem.get('videoThumbnail') + ')',
+		  WebkitTransition: 'all', // note the capital 'W' here
+		  msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+		};
+
 		return (
 			<li>
-				<span>{ playlistItem.get('videoId') }</span>
-				<span>{ playlistItem.get('createdAt').toString() }</span>
-				<span>{ playlistItem.get('updatedAt').toString() }</span>
-				<span>{ playlistItem.get('likes') }</span>
+				<div className="video-pic" style={videoPicStyle}>
+					<div className="video-duration">{ playlistItem.get('videoDuration') }</div>
+				</div>
+				<div className="video-title">{ playlistItem.get('videoTitle') }</div>
+				<div className="video-likes">{ playlistItem.get('likes') }</div>
 			</li>
 		);
 	}
@@ -62,7 +69,7 @@ var PlaylistComponent = React.createClass({
 			return <h3>Fetching playlist...</h3>;
 		}
 		return (
-			<ul>
+			<ul className="list-unstyled" id="queue-list">
 				{ playlistItems.map(function (playlistItem) {
 					return <PlaylistItemComponent key={ playlistItem.id } playlistItem={ playlistItem } />
 				}) }

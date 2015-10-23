ce5ae4434b62f739778a08306887fa012b7a98f8
Felix Sun <fephsun@gmail.com>
2015-10-23 14:45:33 -0700

    Hides menu

--- src/slide-container-component.js
+++ src/slide-container-component.js
@@ -6,7 +6,8 @@ var SlideContainerComponent = React.createClass({
 	},
 	getInitialState: function () {
 		return {
-			slideLeft: false
+			slideLeft: false,
+			hide: true,	// slideLeft and hide shouldn't both be true.
 		};
 	},
 	slideLeft: function () {
@@ -17,9 +18,22 @@ var SlideContainerComponent = React.createClass({
 	componentWillMount: function () {
 		events.on('slide-left', this.slideLeft);
 	},
+	hide: function () {
+		this.setState({
+			hide: true,
+		});
+	},
+	unhide: function () {
+		this.setState({
+			hide: false,
+		});
+	},
 	render: function () {
 		return (
-			<div className={ 'slide-container ' + (this.state.slideLeft ? 'slide-left' : '') }>
+			<div className={ 'slide-container ' + (this.state.slideLeft ? 'slide-left' : '') +
+				(this.state.hide ? ' slide-hidden' : '' ) }
+				onMouseOver={ this.unhide }
+				onMouseOut={ this.hide }>
 				<div className="left">
 					<window.PlaylistComponent playlistId={ this.props.playlistId }/>
 					<button className="btn add-video-button" onClick={ this.slideLeft }>

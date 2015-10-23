861009c6347ea4cf170ee46ba4619fac69e602a4
Stefan Valentin <khalilravanna@gmail.com>
2015-10-23 13:41:27 -0700

    Skip video when hitting right arrow key

--- src/app.js
+++ src/app.js
@@ -1,69 +1,81 @@
-      Parse.initialize('EmxeEeMgQxwUeC42hDPXfHK0tnKoTcLSL5OeZsuy', 'p8cryLkuriL0mTlWCmiKMmy0zRbYeMhGJ5YQFG6k');
+Parse.initialize('EmxeEeMgQxwUeC42hDPXfHK0tnKoTcLSL5OeZsuy', 'p8cryLkuriL0mTlWCmiKMmy0zRbYeMhGJ5YQFG6k');
 
-      var qrcode = new QRCode("qrcode", {
-          text: "http://localhost:9000/",
-          width: 130,
-          height: 130,
-          colorDark : "#000000",
-          colorLight : "#ffffff",
-          correctLevel : QRCode.CorrectLevel.H
-      });
+var qrcode = new QRCode("qrcode", {
+    text: "http://localhost:9000/",
+    width: 130,
+    height: 130,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
 
-      window.initPlayer = function(videoId) {
-        $('#video-container').tubular({
-          videoId: videoId,
-          mute: false,
-          repeat: false
-        });
+window.initPlayer = function(videoId) {
+    $('#video-container').tubular({
+        videoId: videoId,
+        mute: false,
+        repeat: false
+    });
 
-        var oldOnPlayerReady = window.onPlayerReady;
-        window.onPlayerReady = function(e) {
-          oldOnPlayerReady(e);
+    var oldOnPlayerReady = window.onPlayerReady;
+    window.onPlayerReady = function(e) {
+        oldOnPlayerReady(e);
 
-          $(window).keypress(function(e) {
+        $(window).keypress(function(e) {
             if (e.keyCode === 0 || e.keyCode === 32) {
-              // Dont pause if the user is typing text in an input
-              if (e.target.nodeName === 'INPUT') {
-                return;
-              }
-              if (window.player.B.playerState == 1) // playing
-                window.player.pauseVideo();
-              else if (window.player.B.playerState == 2) // paused
-                window.player.playVideo();
+                // Dont pause if the user is typing text in an input
+                if (e.target.nodeName === 'INPUT') {
+                    return;
+                }
+                if (window.player.B.playerState == 1) // playing
+                    window.player.pauseVideo();
+                else if (window.player.B.playerState == 2) // paused
+                    window.player.playVideo();
             }
-          });
-        }
+        });
+    }
 
-        var oldOnPlayerStateChange = window.onPlayerStateChange;
-        window.onPlayerStateChange = function (event) {
-          oldOnPlayerStateChange(event);
-          if (window.player.B.playerState == 0) { // stopped
+    var oldOnPlayerStateChange = window.onPlayerStateChange;
+    window.onPlayerStateChange = function (event) {
+        oldOnPlayerStateChange(event);
+        if (window.player.B.playerState == 0) { // stopped
             var topVideo = window.playlist.getTopVideo();
             window.playlist.removeVideo(topVideo.id);
-          }
-          // else if (window.player.B.playerState == 1) // playing
         }
-      }
+        // else if (window.player.B.playerState == 1) // playing
+    }
+}
 
-      window.playNext = function() {
-        console.log('play next ');
+window.playNext = function() {
+    console.log('play next ');
 
-        var currentVideo = window.playlist.getTopVideo();
-        if (!currentVideo) {
-          return;
-        }
-        currentVideoId = currentVideo.get('videoId');
-        window.playlist.markAsCurrent(currentVideo);
+    var currentVideo = window.playlist.getTopVideo();
+    if (!currentVideo) {
+        return;
+    }
+    currentVideoId = currentVideo.get('videoId');
+    window.playlist.markAsCurrent(currentVideo);
 
-        // announce for 2 seconds
-        window.readme("And now... " + currentVideo.get('videoTitle') + "!");
-        setTimeout(function() {
-          if (window.player === undefined) {
+    // announce for 2 seconds
+    window.readme("And now... " + currentVideo.get('videoTitle') + "!");
+    setTimeout(function() {
+        if (window.player === undefined) {
             window.initPlayer(currentVideoId);
             window.onYouTubeIframeAPIReady();
-          }
-          else {
+        }
+        else {
             window.player.loadVideoById(currentVideoId);
-          }
-        }, 2000);
-      }
+        }
+    }, 2000);
+}
+
+// Skip video when they hit the right arrow key
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 39) {
+        var currentVideo = window.playlist.getTopVideo();
+        if (!currentVideo) {
+            return;
+        }
+        window.playlist.removeVideo(currentVideo.id);
+        window.playNext();
+    }
+});

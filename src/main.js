cd7638a6be5c145b43949da567f8b42414ee4a91
Felix Sun <fephsun@gmail.com>
2015-10-23 10:43:44 -0700

    Random default party names

--- src/main.js
+++ src/main.js
@@ -6,7 +6,23 @@ var ParseTestComponent = window.ParseTestComponent,
 			return {
 				init: false,
 				playlistId: 'you should never be loading this',
+				defaultUsername: '',
 			};
+		},
+		componentDidMount: function() {
+			// This function is called upon load, and currently only loads the
+			// default party name from an external API.
+			$.get('http://randomword.setgetgo.com/get.php', function(result) {
+				this.setState({
+					defaultUsername: result,
+				});
+			}.bind(this));
+		},
+		handleChange: function(event) {
+			// You need this to update a text box, apparently.
+			this.setState({
+				defaultUsername: event.target.value,
+			});
 		},
 		render: function () {
 			if (this.state.init) {
@@ -14,16 +30,19 @@ var ParseTestComponent = window.ParseTestComponent,
 					<window.SlideContainerComponent playlistId ={this.state.playlistId}/>
 				);
 			} else {
+				var playlistName = "testing with spaces";
 				return (
 					<div>
 					<h3> Create a new party with a playlist id, or join an existing
 					one. </h3>
-					<input type='text' name='playlist-id' id='playlist-id'/>
-					<button className='btn existing-playlist-button'
-						onClick={this.existingPlaylist}> Join
-					</button>
+					<input type='text' name='playlist-id' id='playlist-id'
+						value={this.state.defaultUsername}
+						onChange={this.handleChange} />
 					<button className='btn new-playlist-button' onClick={this.newPlaylist}>
 						New
+					</button>
+					<button className='btn existing-playlist-button'
+						onClick={this.existingPlaylist}> Join
 					</button>
 					</div>
 				);
